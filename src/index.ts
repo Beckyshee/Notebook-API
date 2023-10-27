fd5476b1d24eb3c151dabd91ec33db25f0582c81
Beckyshee <wanjirubecky.rw@gmail.com>
2023-10-27 12:03:42 +0300

    delete

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const prisma = new PrismaClient();
 app.use(express.json())
 app.use(cors());
 
-app.get("api/notes", async(req, res)=>{
+app.get("api/notes/:id", async(req, res)=>{
   const notes = await prisma.note.findMany();
   res.json(notes);
 });
@@ -30,6 +30,43 @@ app.post("/api/notes", async (req, res) => {
     res.status(500).json({ error: "An error occurred while creating the note." });
   }
 });
+
+app.put("api/notes/:Id", async(req, res)=>{
+  const { id, title, content, createdAt } =req.body;
+  const Id = parseInt(req.params.Id);
+
+  if(!title || !content) {
+    return res.status(400).send("title and content required")
+  }
+
+  if(!Id || isNaN(Id)) {
+    return res.status(400).send("must be unique")
+  }
+  try {
+    const updatedNote = await prisma.note.update({
+      where: {id},
+      data: {id, title, content, createdAt}
+    })
+    res.json(updatedNote)
+  } catch (error) {
+    res.status(500).send("sth went wrong")
+  }
+});
+
+app.delete("api/notes/:Id", async (req, res) => {
+  const Id = parseInt(req.params.Id);
+  if(!Id || isNaN(Id)) {
+    return res.status(400).send("must be a valid integer")
+  }
+  try {
+    await prisma.note.delete({
+      where: {id},
+    });
+    res.status(400).send("valid integer please");
+  } catch (error) {
+    res.status(500).send("sth went wrong")
+  }
+})
 
 app.listen(4000, ()=>{
   console.log("listening on 4000");
